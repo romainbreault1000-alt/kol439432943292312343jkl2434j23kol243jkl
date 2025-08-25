63c6be87a40f9929002b9cdd9e08611f1d24e685
romainbreault1000-alt <romainbreault.1000@gmail.com>
2025-08-25 16:27:01 -0400

    cleaner.bat

--- Clear_Event_Viewer_Logs.bat
+++ Clear_Event_Viewer_Logs.bat
@@ -1,9 +1,3 @@
-:: Created by: Shawn Brink
-:: Created on: August 15th 2016
-:: Updated on: July 25th 2018
-:: Tutorial: https://www.tenforums.com/tutorials/16588-clear-all-event-logs-event-viewer-windows.html
-
-
 @echo off
 
 FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
@@ -25,4 +19,5 @@ echo Exit now, right click on this .BAT file, and select "Run as administrator".
 pause >nul
 
 :theEnd
+
 Exit
